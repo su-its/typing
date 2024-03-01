3732cd36595b6b7ded4df692a7283f371b3fc2ed
shin0729 <shin.yamaguchi0801@gmail.com>
2024-03-01 15:36:08 +0900

    feat: change user id to UUID

--- typing-server/domain/repository/ent/schema/user.go
+++ typing-server/domain/repository/ent/schema/user.go
@@ -2,9 +2,9 @@ package schema
 
 import (
 	"entgo.io/ent"
-
 	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
 )
 
 // User holds the schema definition for the User entity.
@@ -16,6 +16,8 @@ type User struct {
 func (User) Fields() []ent.Field {
 	//カラムとしてvarcher型(255)のMailAdress,varcher型(255)のHandleName,varcher型(255)のName,varcher型(255)のHashedPassword,enum型のDepartment,datetime型のCreatedAt,datetime型のUpdatedAtを持たせる
 	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(func() uuid.UUID { return uuid.Must(uuid.NewRandom()) }),
 		field.String("MailAdress").NotEmpty().MaxLen(255),
 		field.String("HandleName").NotEmpty().MaxLen(255),
 		field.String("Name").NotEmpty().MaxLen(255),
