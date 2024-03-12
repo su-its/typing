02708fdb66da0613a9a0690e9de036696e96b372
Sora Arakawa <mail@arkw.net>
2024-03-12 11:26:48 +0900

    update: headerでユーザ情報を表示

--- typing-app/src/components/organism/header/index.tsx
+++ typing-app/src/components/organism/header/index.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import "../../../styles/header.scss";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="/img/header_logo.png" />
+      </div>
+      <div className="user">
+        <div className="icon">
+          <img src="/img/icon_default.png" />
+        </div>
+        <div className="name">名称未設定</div>
+        <div className="number">
+          <div className="tip">Student No.</div>
+          <div className="value">00000000</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
