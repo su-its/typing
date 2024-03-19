544f59a32cef77d62978eeced440956f619aedda
i2rin <tangairin3@gmail.com>
2024-03-19 18:41:24 +0900

    feat: 時間経過のバーを実装

--- typing-app/src/components/templates/GameTyping.tsx
+++ typing-app/src/components/templates/GameTyping.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Text, Progress } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import { SubGamePageProps } from "../pages/Game";
 
 const GameTyping: React.FC<SubGamePageProps> = ({ nextPage }) => {
-  const [count, setCount] = useState(5); //仮で5秒に設定
+  const totalSeconds = 20;
+  const [count, setCount] = useState(totalSeconds);
 
   useEffect(() => {
     const timer = count > 0 && setInterval(() => setCount(count - 1), 1000);
@@ -13,10 +14,13 @@ const GameTyping: React.FC<SubGamePageProps> = ({ nextPage }) => {
     return () => clearInterval(timer);
   }, [count, nextPage]);
 
+  count progress = ((totalSeconds - count ) / totalSeconds) * 100;
+
   return (
     <Box>
       <Text>Typing screen</Text>
       <Button onClick={nextPage}>finish</Button>
+      <Progress value={progress} colorScheme = "blue" />
     </Box>
   );
 };
