334f36bafdacb75d9b6e8ea9fe258fbc995b195a
i2rin <tangairin3@gmail.com>
2024-03-19 18:31:10 +0900

    feat: タイマー機能を実装

--- typing-app/src/components/templates/GameTyping.tsx
+++ typing-app/src/components/templates/GameTyping.tsx
@@ -1,8 +1,18 @@
 import { Box, Button, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SubGamePageProps } from "../pages/Game";
 
 const GameTyping: React.FC<SubGamePageProps> = ({ nextPage }) => {
+  const [count, setCount] = useState(5); //仮で5秒に設定
+
+  useEffect(() => {
+    const timer = count > 0 && setInterval(() => setCount(count - 1), 1000);
+    if (cout === 0) {
+      nextPage();
+    }
+    return () => clearInterval(timer);
+  }, [count, nextPage]);
+
   return (
     <Box>
       <Text>Typing screen</Text>
