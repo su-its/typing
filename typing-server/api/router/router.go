d02645bb33559096e97e883ecc1bbc53d29218b3
KinjiKawaguchi <kawakin0310@icloud.com>
2024-03-23 16:31:18 +0900

    refactor: ヘルスチェック関連のコードの位置を変更

--- typing-server/api/router/router.go
+++ typing-server/api/router/router.go
@@ -4,14 +4,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/su-its/typing/typing-server/api/controller/system"
 	"github.com/su-its/typing/typing-server/api/handler"
 )
 
 func SetupRouter() http.Handler {
 	r := chi.NewRouter()
 
-	r.Get("/health", system.HealthCheck)
+	r.Get("/health", handler.HealthCheck)
 
 	r.Get("/users", handler.GetUsers)
 
