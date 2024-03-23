31689ddaa6cb74f56da369414890c7b1bfa0ba24
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-03-23 10:18:06 +0900

    chore:コンポーネントの配置を変更

--- typing-app/src/components/pages/Home.tsx
+++ typing-app/src/components/pages/Home.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Text, VStack } from "@chakra-ui/react";
+import GameStartButton from "../buttons/gameStartButton";
+import RankingButton from "../buttons/rankingButton";
+import LogoutButton from "../buttons/logoutButton";
 
 const HomePage: React.FC = () => {
   return (
@@ -7,6 +10,9 @@ const HomePage: React.FC = () => {
       <VStack>
         <Text fontSize="2xl">Hello, World!</Text>
         <Text fontSize="xl">Welcome to the ITS Room</Text>
+        <GameStartButton />
+        <RankingButton />
+        <LogoutButton />
       </VStack>
     </>
   );
