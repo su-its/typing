82f7a323425902aad5935cb7d42aef7932ebd88b
KinjiKawaguchi <kawakin0310@icloud.com>
2024-03-25 00:59:34 +0900

    feat: モデルファイルを追加

---
+++ typing-server/domain/model/model.go
@@ -0,0 +1,24 @@
+package model
+
+import "time"
+
+type ScoreRanking struct {
+	Rank  int   `json:"rank"`
+	Score Score `json:"score"`
+}
+
+type User struct {
+	ID            string    `json:"id"`
+	StudentNumber string    `json:"student_number"`
+	HandleName    string    `json:"handle_name"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
+
+type Score struct {
+	ID         string    `json:"id"`
+	Keystrokes int       `json:"keystrokes"`
+	Accuracy   float64   `json:"accuracy"`
+	CreatedAt  time.Time `json:"created_at"`
+	User       User      `json:"user"`
+}
