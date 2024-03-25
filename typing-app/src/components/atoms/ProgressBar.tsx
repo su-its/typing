63a6c2571349c82bf4709ae7e1f44114413ce18f
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-03-25 19:39:40 +0900

    feat: ProgressBar変更時のアニメーションを追加

--- typing-app/src/components/atoms/ProgressBar.tsx
+++ typing-app/src/components/atoms/ProgressBar.tsx
@@ -11,7 +11,11 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ maxWidth, value, maxValue, he
   const rate = value / maxValue;
   const actualWidth = maxWidth * rate + "px";
   const actualHeight = height + "px";
-  return <div style={{ background: "#2196f3", height: actualHeight, width: actualWidth }} />;
+  return (
+    <div
+      style={{ background: "#2196f3", height: actualHeight, width: actualWidth, transition: "width 0.2s ease-in-out" }}
+    />
+  );
 };
 
 export default ProgressBar;
