e330ca78007446fe8f1aa155ceebcdf974f4e415
わか <100208180+waka-eup25@users.noreply.github.com>
2024-03-28 23:32:22 +0900

    fix: 入力時間の表示フォーマット修正

--- typing-app/src/components/templates/GameResult.tsx
+++ typing-app/src/components/templates/GameResult.tsx
@@ -63,8 +63,8 @@ const GameResult: React.FC<GameResultProps> = ({ nextPage, resultScore }) => {
         </GridItem>
         <GridItem colSpan={3} colStart={6} rowStart={5} className={styles.centerText}>
           <Text fontSize="2xl" as="b">
-            {resultScore.time.getMinutes()}分{resultScore.time.getSeconds()}秒
-            {Math.floor(resultScore.time.getMilliseconds() / 100)}
+          {String(resultScore.time.getMinutes() * 60 + resultScore.time.getSeconds()).padStart(2, '0')}秒
+          {String(Math.floor(resultScore.time.getMilliseconds() / 100))}
           </Text>
         </GridItem>
         <GridItem colSpan={3} colStart={6} rowStart={6} className={styles.centerText}>
