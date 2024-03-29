77ce97ce062c0a27185fdf881016a4c714a8c451
Shibatch <lawnbone.tubist@gmail.com>
2024-03-29 23:30:15 +0900

    サンプルテキストを増やした

--- typing-app/src/app/game/page.tsx
+++ typing-app/src/app/game/page.tsx
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 export default function Typing() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
   // テキストファイルのパスを指定
   const filePath = path.join(process.cwd(), 'src/assets/texts', `text${randomNumber}.txt`);
   // ファイルの内容を同期的に読み込んで fileContent に格納
