5e7bd3c0b151c4b351657aeaaae9d9ef26948d43
Shibatch <lawnbone.tubist@gmail.com>
2024-04-01 19:42:19 +0900

    fix: 乱数の生成範囲のミスを修正

--- typing-app/src/app/game/page.tsx
+++ typing-app/src/app/game/page.tsx
@@ -3,7 +3,7 @@ import fs from "fs";
 
 export default function Typing() {
   const filenames = fs.readdirSync("src/assets/texts/");
-  const randomNumber = Math.floor(Math.random() * filenames.length) + 1; //
+  const randomNumber = Math.floor(Math.random() * filenames.length); //
   const subjectText = fs.readFileSync(`src/assets/texts/text${randomNumber}.txt`, "utf-8");
 
   return <GamePage subjectText={subjectText} />;
