923e2d5ef0994d662a4427e0d0470475083b37e3
Shibatch <lawnbone.tubist@gmail.com>
2024-04-01 21:40:20 +0900

    style: ゲーム開始前の画面をスタイリング

--- typing-app/src/components/templates/GamePre.tsx
+++ typing-app/src/components/templates/GamePre.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { GamePreProps } from "../pages/Game";
 import styles from "./GamePre.module.css";
@@ -24,9 +24,34 @@ const GamePre: React.FC<GamePreProps> = ({ nextPage }) => {
   return (
     <Box>
       <div className={styles.box}>
-        <Text fontSize="4xl">
-          ゲーム説明
-        </Text>
+        <Grid
+          templateAreas={`"header header"
+                          "main nav"
+                          "footer footer"`}
+          gridTemplateRows={"60px 1fr 50px"}
+          gridTemplateColumns={"1fr 300px"}
+          h="100%"
+          gap={6}
+          bg="blackAlpha"
+          border="4px solid white"
+        >
+          <GridItem pl="2" area={"header"} className={styles.centerText}>
+            <Text color="white" fontSize="4xl" as="b">
+              ゲーム説明
+            </Text>
+          </GridItem>
+          <GridItem pl="4" color="white" area={"main"}>
+            <Text>本文</Text>
+          </GridItem>
+          <GridItem pr="4" color="white" area={"nav"}>
+            <Text>キーボードの写真とか？</Text>
+          </GridItem>
+          <GridItem pl="2" area={"footer"} className={styles.centerText}>
+            <Text color="white" fontSize="3xl">
+              [Space]キーを押して開始
+            </Text>
+          </GridItem>
+        </Grid>
       </div>
     </Box>
   );
