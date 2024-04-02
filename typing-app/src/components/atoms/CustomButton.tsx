ec3193e3019d0646d845cfec4a07d5b78cda599d
araaki12345 <74499464+araaki12345@users.noreply.github.com>
2024-04-02 17:27:24 +0900

    style:ランキング画面の配置とボタン色の調整

--- typing-app/src/components/atoms/CustomButton.tsx
+++ typing-app/src/components/atoms/CustomButton.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 export const CustomButton = ({ onClick, isDisabled, children }: ButtonProps) => {
   return (
-    <Button onClick={onClick} isDisabled={isDisabled}>
+    <Button bg={"#2B6CB0"} color={"white"} onClick={onClick} isDisabled={isDisabled}>
       {children}
     </Button>
   );
--- typing-app/src/components/molecules/RankingTableRow.tsx
+++ typing-app/src/components/molecules/RankingTableRow.tsx
@@ -5,7 +5,7 @@ const RankingTableRow: React.FC<ScoreRanking> = (scoreRanking) => {
   return (
     <Tr key={String(scoreRanking.rank)} _even={{bg: 'midnightblue'}} _odd={{bg: '#192f70'}} color={'silver'}>
       <Td width={"128px"} textAlign={"center"}>{String(scoreRanking.rank)}</Td>
-      <Td isNumeric width={"256px"} textAlign={"center"}>{scoreRanking.user.studentNumber}</Td>
+      <Td width={"256px"} textAlign={"center"}>{scoreRanking.user.studentNumber}</Td>
       <Td width={"320px"} textAlign={"center"}>{String(scoreRanking.keystrokes)}</Td>
       <Td width={"256px"} textAlign={"center"}>{String(scoreRanking.accuracy)}</Td>
       <Td width={"320px"} textAlign={"center"}>{scoreRanking.createdAt.toLocaleDateString("ja-JP")}</Td>
