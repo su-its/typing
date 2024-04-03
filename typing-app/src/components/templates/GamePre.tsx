dfb43ca7ed83c9c3e0f843bc6c2ec25e0f28b235
Shibatch <lawnbone.tubist@gmail.com>
2024-04-03 15:26:44 +0900

    docs: 英訳を削除

--- typing-app/src/components/templates/GamePre.tsx
+++ typing-app/src/components/templates/GamePre.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, GridItem, Text, Image, Center } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Text, Image, Center, Flex } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { GamePreProps } from "../pages/Game";
 import styles from "./GamePre.module.css";
@@ -42,16 +42,12 @@ const GamePre: React.FC<GamePreProps> = ({ nextPage }) => {
             </Text>
           </GridItem>
           <GridItem pl="4" color="white" area={"main"} fontSize="2xl">
-            <Text>制限時間は1分間!</Text>
-            <Text>英文を速く・正確に入力して高スコアを目指そう！</Text>
-            <Text>【入力スコア目安】</Text>
-            <Text>WPM(打鍵数): 120字以上</Text>
-            <Text mb={6}>正打率: 95%以上</Text>
-            <Text>The time limit is 1 minute!</Text>
-            <Text>Enter English sentences quickly and accurately to achieve your best score!</Text>
-            <Text>【Suggested Scores】</Text>
-            <Text>WPM(Words Per Minutes): 120 words or more</Text>
-            <Text>Accuracy: 95% or more</Text>
+            <Box height="100%" alignItems="center">
+              <Text>制限時間は1分間!</Text>
+              <Text>英文を速く・正確に入力して高スコアを目指そう！</Text>
+              <Text>【ランキング掲載条件】</Text>
+              <Text>WPM(打鍵数): 120字以上 かつ 正打率: 95%以上</Text>
+            </Box>
           </GridItem>
           <GridItem pr="4" color="white" area={"nav"} height="100%" placeItems="center">
             <Center mt="20">
@@ -60,7 +56,7 @@ const GamePre: React.FC<GamePreProps> = ({ nextPage }) => {
           </GridItem>
           <GridItem pl="2" area={"footer"} className={styles.centerText}>
             <Text color="white" fontSize="3xl">
-              [Space]キーを押して開始 / Press [Space] to start
+              [Space]キーを押して開始
             </Text>
           </GridItem>
         </Grid>
