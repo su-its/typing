40388df729d1116bbf245143498d13fa32757603
Sora Arakawa <mail@arkw.net>
2024-04-07 16:20:54 +0900

    remove: Google Fontsに依存する書体を削除

--- typing-app/src/app/layout.tsx
+++ typing-app/src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Header from "../components/organism/Header";
 import Footer from "../components/organism/Footer";
 import "./globals.css";
 import { Box, ChakraProvider } from "@chakra-ui/react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "TYPE MASTER",
@@ -18,7 +15,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ChakraProvider>
           <Box minH="100vh" display="flex" flexDirection="column" backgroundImage="url('/img/background.png');">
             <Header />
