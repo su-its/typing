e69fbae70323bf52141ed8829e0cd3d4836d1655
Sora Arakawa <mail@arkw.net>
2024-04-09 22:11:08 +0900

    move: 背景画像をassetsディレクトリへ移動

--- typing-app/src/app/layout.tsx
+++ typing-app/src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "../components/organism/Header";
 import Footer from "../components/organism/Footer";
 import "./globals.css";
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import background from "@/assets/images/background.png";
 
 export const metadata: Metadata = {
   title: "TYPE MASTER",
@@ -17,7 +18,7 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <ChakraProvider>
-          <Box minH="100vh" display="flex" flexDirection="column" backgroundImage="url('/img/background.png');">
+          <Box minH="100vh" display="flex" flexDirection="column" backgroundImage={background.src}>
             <Header />
             <Box flex="1" py={2}>
               {children}
