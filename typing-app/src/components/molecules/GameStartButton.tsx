cafeb5e5b052093867f732477ee96848253c91b2
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-04-18 02:20:23 +0900

    feat: ログイン時にモーダルを表示しない

--- typing-app/src/components/molecules/GameStartButton.tsx
+++ typing-app/src/components/molecules/GameStartButton.tsx
@@ -4,13 +4,25 @@ import React from "react";
 import { Image, useDisclosure } from "@chakra-ui/react";
 import LoginModal from "./LoginModal";
 import gameButton from "@/assets/images/home/game.png";
+import { getCurrentUser } from "@/app/actions";
+import { useRouter } from "next/navigation";
+
 
 const GameStartButton = () => {
+  const router = useRouter();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const onClick = async () => {
+    const user = await getCurrentUser();
+    if (user) {
+      router.push("/game");
+    } else {
+      onOpen();
+    }
+  }
 
   return (
     <>
-      <Image mb={2} src={gameButton.src} onClick={onOpen} cursor="pointer" />
+      <Image mb={2} src={gameButton.src} onClick={onClick} cursor="pointer" />
       <LoginModal isOpen={isOpen} onClose={onClose} />
     </>
   );
