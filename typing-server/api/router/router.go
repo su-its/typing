9b84a7e5c7ce2c342b3d8fa1c2ea22fd1d4848ba
KinjiKawaguchi <kawakin0310@icloud.com>
2024-12-12 07:00:21 +0900

    feat(backend): Add environment configuration support and update Dockerfiles

--- typing-server/api/router/router.go
+++ typing-server/api/router/router.go
@@ -1,24 +1,26 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/cors"
+	"github.com/su-its/typing/typing-server/api/config"
 	"github.com/su-its/typing/typing-server/api/handler"
 )
 
-func SetupRouter() http.Handler {
+func SetupRouter(config *config.Config) http.Handler {
 	r := chi.NewRouter()
 
 	cors := cors.New(cors.Options{
-		AllowedOrigins: getAllowedOrigins(),
+		AllowedOrigins: getAllowedOrigins(config.Environment),
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders: []string{"Link"},
 		MaxAge:         300, // Preflightリクエストの結果をキャッシュする時間（秒）
 	})
+	fmt.Println(getAllowedOrigins(config.Environment))
 	r.Use(cors.Handler)
 
 	r.Get("/health", handler.HealthCheck)
@@ -31,9 +33,15 @@ func SetupRouter() http.Handler {
 	return r
 }
 
-func getAllowedOrigins() []string {
-	if os.Getenv("DEV_MODE") == "true" {
-		return []string{"http://localhost:*", "http://ty.inf.in.shizuoka.ac.jp"}
+func getAllowedOrigins(enviroment string) []string {
+	if enviroment == "local" {
+		return []string{
+			"http://localhost:3000",
+			"http://127.0.0.1:3000",
+		}
 	}
-	return []string{"http://ty.inf.in.shizuoka.ac.jp"}
+	return []string{
+		"http://ty.inf.in.shizuoka.ac.jp",
+		"https://ty.inf.in.shizuoka.ac.jp",
+	}
 }
