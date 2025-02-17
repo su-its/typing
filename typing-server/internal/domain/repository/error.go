bf85b17177d1964fb6aafc7a699f4c4cbd7ae37d
shgnaka <shogonakamurawppt@gmail.com>
2025-02-17 18:24:14 +0900

    feat: CreateUser functionを定義，学籍番号が重複した場合のエラーを実装，CreateUserインターフェースを作成

---
+++ typing-server/internal/domain/repository/error.go
@@ -0,0 +1,7 @@
+package repository
+
+import "errors"
+
+var (
+	ErrAlreadyExists = errors.New("student number already exists")
+)
--- typing-server/internal/domain/repository/user_repository.go
+++ typing-server/internal/domain/repository/user_repository.go
@@ -10,4 +10,7 @@ type UserRepository interface {
 	// GetUserByStudentNumber は、指定された学籍番号を持つユーザーを取得する。
 	// 該当するユーザーが存在しない場合は、(nil, nil) を返す。
 	GetUserByStudentNumber(ctx context.Context, studentNumber string) (*model.User, error)
+	// CreateUser は、指定された学籍番号とハンドルネームを持つユーザーを作成する。
+	// 既に同じ学籍番号を持つユーザーが存在する場合は、{nil, ErrAlreadyExists} を返す。
+	CreateUser(ctx context.Context, studentNumber string, handleName string) (*model.User, error)
 }
