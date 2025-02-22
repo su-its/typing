6aaf970a16b2a58cedaf71449d1307efb743d65d
Sora Arakawa <mail@arkw.net>
2025-02-22 13:03:52 +0900

    refactor: Headerと関連コンポーネントのChakra依存を削除 (wip)

--- typing-app/src/components/molecules/UserCard.tsx
+++ typing-app/src/components/molecules/UserCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Avatar, Text, HStack, VStack, Spacer } from "@chakra-ui/react";
 import type { StackProps } from "@chakra-ui/react";
 import { getCurrentUser } from "@/app/actions";
 import type { User } from "@/types/user";
+import styles from "@/assets/sass/molecules/UserCard.module.scss";
 
 interface UserCardPresenterProps extends StackProps {
   user?: User;
@@ -15,21 +15,17 @@ export const UserCardPresenter = ({ user, ...rest }: UserCardPresenterProps) => 
   };
 
   return (
-    <HStack spacing={4} bg="blue.600" {...props}>
-      <Avatar
-        src={"https://www.shizuoka.ac.jp/cms/files/shizudai/MASTER/0100/uISrbYCb_VL033_r03.png"}
-        boxSize="70px"
-        borderRadius="0"
-      />
-      <VStack align="start" overflow="hidden" flexGrow={1} gap={0}>
-        <Text fontSize="lg" fontWeight="bold" color="white" isTruncated width="90%">
-          名前: {user ? user.handleName : "ログインしていません"}
-        </Text>
-        <Text color="white" width="90%" isTruncated>
-          学籍番号: {user ? user.studentNumber : "未ログイン"}
-        </Text>
-      </VStack>
-    </HStack>
+    <>
+      <div className={styles["user-card"]}>
+        <div className={styles.left}>
+          <img src={"https://www.shizuoka.ac.jp/cms/files/shizudai/MASTER/0100/uISrbYCb_VL033_r03.png"} />
+        </div>
+        <div className={styles.right}>
+          <div className={styles.name}>{user ? user.handleName : "ログインしていません"}</div>
+          <div className={styles.number}>学籍番号: {user ? user.studentNumber : ""}</div>
+        </div>
+      </div>
+    </>
   );
 };
 
