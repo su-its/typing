43f6e05b32733542cfc675d14aa6ac261664f549
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2025-04-06 14:16:39 +0900

    fix: formのサブミットが動かないのを場当たり的に修正

--- typing-app/src/components/molecules/GameStartButton.tsx
+++ typing-app/src/components/molecules/GameStartButton.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React from "react";
-import { useDisclosure } from "@chakra-ui/react";
+import React, { useState } from "react";
 import LoginModal from "./LoginModal";
 import gameButton from "@/assets/images/home/game.png";
 import { getCurrentUser } from "@/app/actions";
@@ -10,13 +9,13 @@ import styles from "@/assets/sass/molecules/GameStartButton.module.scss";
 
 const GameStartButton = () => {
   const router = useRouter();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isOpen, setOpen] = useState(false);
   const onClick = async () => {
     const user = await getCurrentUser();
     if (user) {
       router.push("/game");
     } else {
-      onOpen();
+      setOpen(true);
     }
   };
 
@@ -26,7 +25,7 @@ const GameStartButton = () => {
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={gameButton.src} alt="" />
       </div>
-      <LoginModal isOpen={isOpen} onClose={onClose} />
+      <LoginModal isOpen={isOpen} onClose={() => setOpen(false)} />
     </>
   );
 };
