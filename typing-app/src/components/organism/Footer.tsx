958cdadd83996a41ff64eddbe394bb56eacd1443
Sora Arakawa <mail@arkw.net>
2025-04-10 22:36:33 +0900

    fix: フッタのJSXから余分な記述を削除

--- typing-app/src/components/organism/Footer.tsx
+++ typing-app/src/components/organism/Footer.tsx
@@ -7,18 +7,14 @@ const date = new Date();
 
 const Footer: React.FC = () => {
   return (
-    <>
-      <div className={styles.footer}>
-        <Separator />
-        <div className={styles["brand-text"]}>
-          <BrandText />
-        </div>
-        <div className={styles.text}>
-          (c) 2024-{date.getFullYear()} Faculty of Informatics, Shizuoka University all rights reserved. Developed by IT
-          Solution Room, Shizuoka University.
-        </div>
+    <div className={styles.footer}>
+      <Separator />
+      <BrandText />
+      <div className={styles.text}>
+        (c) 2024-{date.getFullYear()} Faculty of Informatics, Shizuoka University all rights reserved. Developed by IT
+        Solution Room, Shizuoka University.
       </div>
-    </>
+    </div>
   );
 };
 
